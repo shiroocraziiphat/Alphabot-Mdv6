928d991cc8b65b6b801caedb3b0f1722821e64e0
AdeurX <98345064+AdeurX@users.noreply.github.com>
2022-02-13 13:13:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "HirzBOTz"
-global.ownername= "ᴹᴿ᭄ HirzBOTz ×፝֟͜×"
+global.botname = "Shiro-Md"
+global.ownername= "shiro"
 global.myweb ="https://api-alphabot.herokuapp.com/"
 global.youtube = "https://youtube.com/c/ZeeoneOfc"
-global.github = "https://HirzBOTz.github.io/"
-global.ownernomer = "6283804421309"
-global.ownernomerr = "6283804421309"
+global.github = "https://AdeurX.github.io/"
+global.ownernomer = "6282361160044"
+global.ownernomerr = "6282361160044"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/298462103666a1e7bc6cf.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6283804421309","6283804421309","6283804421309"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© HirzBOTz' //sticker wm
-global.author = 'Di Buat Oleh HirzOfc' //sticker wm
+global.owner = ["6282361160044","62882361160044","6282361160044"] //ganti agar fitur owner bisa di gunakan
+global.packname = '©shiro-md' //sticker wm
+global.author = 'lovyuuuuu<3' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -40,7 +40,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
+    sukses: 'Done, Oke Desu><',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
     owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
@@ -48,7 +48,7 @@ global.mess = {
     private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
 	bot: 'Fitur Khusus Pengguna Nomor Bot',
     errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
+    wait: 'Sedang Di Proses :)',
 	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
